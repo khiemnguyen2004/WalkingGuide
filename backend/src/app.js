2fe7991a5421f59fdc87a5ee2dd0f2b1d0fc4628
KhiemCho <khiem.nhg.64cntt@ntu.edu.vn>
2025-07-23 09:11:41 +0700

    update planner

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
-const placeController = require("./controllers/placeController");
-app.use("/api/places", placeController);
-const bookingRoutes = require('./routes/bookingRoutes');
-app.use('/api/bookings', bookingRoutes);
+// Comment out or remove all code in this file to prevent accidental double app definition or route conflicts.
+// const placeController = require("./controllers/placeController");
+// app.use("/api/places", placeController);
+// const bookingRoutes = require('./routes/bookingRoutes');
+// app.use('/api/bookings', bookingRoutes);
